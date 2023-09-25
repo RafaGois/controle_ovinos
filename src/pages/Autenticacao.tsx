3603bc696e4a752f1db478e7f837c35f75c4589f
Rafael Fontana Becker Gois <joefontanabg@gmail.com>
2023-09-24 21:29:32 -0300

    initial commit

---
+++ src/pages/Autenticacao.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Autenticacao () {
+    return (
+        <div className="h-screen flex">
+            <div className="h-full lg:w-2/3 md:w-1/2 bg-[url('../../public/ovelhas.jpg')]"></div>
+            <div className="h-full w-1/3 md:w-1/2 sm:w-full bg-white"></div>
+        </div>
+    )
+}
